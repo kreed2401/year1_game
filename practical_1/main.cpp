0b88c0213e0e28df59b2b2d61fa33523a3888405
kreed2401 <58709017+kreed2401@users.noreply.github.com>
2021-02-02 14:06:31 +0000

    MadePong

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
 
 using namespace sf;
 using namespace std;
@@ -17,8 +19,19 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 
+int p1Score = 0;
+int p2Score = 0;
+
+Vector2f ballVelocity;
+bool server = false;
+
+bool aiControlled = false;
+
 CircleShape ball;
 RectangleShape paddles[2];
+
+sf::Font font;
+sf::Text text;
 
 void Load() 
 {
@@ -29,23 +42,92 @@ void Load()
         p.setOrigin(paddleSize / 2.f);
     }
     //Initilize Ball
-    ball.setRadius(ballRadius - 3);
-    ball.setOrigin(ballRadius / 2. ballRadius / 2);
+    ball.setRadius(ballRadius);
+    ball.setOrigin(ballRadius / 2, ballRadius / 2);
     paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-    paddles[1].setPosition(gameWidth.x - 10, paddleSize.x / 2, gameHeight / 2);
+    paddles[1].setPosition(gameWidth - 30 + paddleSize.x / 2, gameHeight / 2);
 
-    ball.setPosition(gameWidth.x / 2, ballRadius, gameHeight / 2);
+    ball.setPosition(gameWidth / 2 + ballRadius, gameHeight / 2);
+    ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+
+    //Initilize Score
+    font.loadFromFile("res/RobotoMono-VariableFont_wght.ttf");
+    text.setFont(font);
+    text.setCharacterSize(24);
+
+    text.setString(p1Score + "|" + p2Score);
+    text.setPosition((gameWidth * .5f) - (text.getLocalBounds().width * .5f), 0);
 }
 
-void Update(RenderWindow &window)
+void Reset()
+{
+    std::cout << "Reset";
+    ball.setPosition(gameWidth / 2 + ballRadius, gameHeight / 2);
+    ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+    text.setString(p1Score + "|" + p2Score);
+}
+
+void ballCollision()
+{
+    //Ball Collision
+    const float bx = ball.getPosition().x;
+    const float by = ball.getPosition().y;
+
+    if (by > gameHeight)
+    {
+        ballVelocity.x *= 1.1f;
+        ballVelocity.y *= -1.1f;
+        ball.move(0, -10);
+    }
+    else if (by < 0)
+    {
+        ballVelocity.x *= 1.1f;
+        ballVelocity.y *= -1.1f;
+        ball.move(0, 10);
+    }
+    else if (bx > gameWidth)
+    {
+        std::cout << "RightWall Reset";
+        Reset();
+        std::cout << bx;
+        p1Score++;
+    }
+    else if (bx < 0)
+    {
+        std::cout << "LeftWall Reset";
+        Reset();
+        p2Score++;
+    }
+    else if (bx < paddleSize.x
+        && by > paddles[0].getPosition().y - (paddleSize.y * 0.5)
+        && by < paddles[0].getPosition().y + (paddleSize.y * 0.5))
+    {
+        std::cout << "Boing";
+        ballVelocity.x *= -1.1f;
+        ballVelocity.y *= 1.1f;
+        ball.move(20, 0);
+        std::cout << bx;
+    }
+    else if (bx > gameWidth - paddleSize.x
+        && by > paddles[1].getPosition().y - (paddleSize.y * 0.5)
+        && by < paddles[1].getPosition().y + (paddleSize.y * 0.5))
+    {
+        std::cout << "Boing";
+        ballVelocity.x *= -1.1f;
+        ballVelocity.y *= 1.1f;
+        ball.move(-20, 0);
+    }
+}
+
+void Update(RenderWindow& window)
 {
     static Clock clock;
-    float dt = clock.restart() / asSeconds();
+    float dt = clock.restart().asSeconds();
 
     Event event;
-    while(window.pollEvent(event))
+    while (window.pollEvent(event))
     {
-        if(event.type == Event::Closed)
+        if (event.type == Event::Closed)
         {
             window.close();
             return;
@@ -53,24 +135,70 @@ void Update(RenderWindow &window)
     }
 
     if (Keyboard::isKeyPressed(Keyboard::Escape)) window.close();
+    if (Keyboard::isKeyPressed(Keyboard::I)) aiControlled = !aiControlled;
 
     float direction = 0.0f;
-    if (Keyboard::isKeyPressed(controls[0])) 
+    float p2Direction = 0.0f;
+    if (Keyboard::isKeyPressed(controls[0]))
     {
-        direction--;
+        if (paddles[0].getPosition().y - 50 > 0) 
+        {
+            direction--;
+        }
     }
-    if(Keyboard::isKeyPressed(controls[1]))
+    if (Keyboard::isKeyPressed(controls[1]))
     {
-        direction++;
+        if (paddles[0].getPosition().y + 50 < gameHeight)
+        {
+            direction++;
+        }
     }
-    paddles[0].move(0, direction * paddlesSpeed * dt);
+    if (Keyboard::isKeyPressed(controls[2]) && !aiControlled)
+    {
+        if (paddles[1].getPosition().y - 50 > 0)
+        {
+            p2Direction--;
+        }
+    }
+    if (Keyboard::isKeyPressed(controls[3]) && !aiControlled)
+    {
+        if (paddles[1].getPosition().y + 50 < gameHeight)
+        {
+            p2Direction++;
+        }
+    }
+
+    paddles[0].move(0, direction * paddleSpeed * dt);
+    paddles[1].move(0, p2Direction * paddleSpeed * dt);
+
+    ball.move(ballVelocity * dt);
+
+    ballCollision();
+
+    //Control the P2 with AI
+    if (aiControlled) 
+    {
+            if (ball.getPosition().y > paddles[1].getPosition().y)
+            {
+                paddles[1].move(0, 1 * paddleSpeed * dt);
+            }
+            else
+            {
+                paddles[1].move(0, -1 * paddleSpeed * dt);
+            }
+    }
+    
+
 }
+
+
 
 void Render(RenderWindow &window)
 {
     window.draw(paddles[0]);
     window.draw(paddles[1]);
     window.draw(ball);
+    window.draw(text);
 }
 
 int main(){
@@ -78,7 +206,7 @@ int main(){
     Load();
     while (window.isOpen()) 
     {
-        window.clear;
+        window.clear();
         Update(window);
         Render(window);
         window.display();
